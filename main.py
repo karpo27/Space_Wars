61b56f3617ad8b6c840c4726a2762a7047525bb4
Julian Giudice <juliangiudice@hotmail.com>
2022-12-28 16:10:38 -0300

    Created title, icon and player function

--- main.py
+++ main.py
@@ -11,16 +11,38 @@ pygame.init()
 size = width, height = (800, 800)
 screen = pygame.display.set_mode(size)
 
+# Title and Icon
+pygame.display.set_caption("Game_Project")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+# Player
+player_img = pygame.image.load('player_img.png')
+player_x = 370
+player_y = 480
+
+
+def player():
+    screen.blit(player_img, (player_x, player_y))
+
+
 # Game Loop
 running = True
 while running:
+    # Screen - RGB
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    player()
+
+    pygame.display.update()
+
+
+
 pygame.quit()
-
-
 
 if __name__ == '__main__':
     pass
