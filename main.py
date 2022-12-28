62656983a4656362ee47d4b6e0ed355e859db262
Julian Giudice <juliangiudice@hotmail.com>
2022-12-28 15:27:03 -0300

    Created pygame loop and screen

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+# Scripts
 
 
+# Modules
+import pygame
 
+# Initialize Pygame
+pygame.init()
 
+# Create the screen
+size = width, height = (800, 800)
+screen = pygame.display.set_mode(size)
 
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
 
 
 
