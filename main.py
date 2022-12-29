b5ea3301a5eabbb5838af9e115b6d8edbead1c6e
Julian Giudice <juliangiudice@hotmail.com>
2022-12-29 16:40:31 -0300

    Created player bullet and bullet movement

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ player_y = 480
 player_xΔ = 0
 player_yΔ = 0
 
+# Player Bullet
+bullet_img = pygame.image.load('bullets.png')
+l_bullet = 64
+bullet_x = 0
+bullet_y = 480
+bullet_xΔ = 0
+bullet_yΔ = 1.5
+bullet_state = "ready"  # At this state we can't see bullet on screen
+
 # Enemy
 enemy_img = pygame.image.load('enemy_img.png')
 l_enemy = 64
@@ -41,6 +50,13 @@ def show_player(x, y):
 def show_enemy(x, y):
     screen.blit(enemy_img, (x, y))
 
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bullet_img, (x + 16, y + 10))
+
+
 # Game Loop
 running = True
 while running:
@@ -51,17 +67,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # Player Keyboard Movement
+        # Press Keyboard
         if event.type == pygame.KEYDOWN:
+            # Player Keyboard Movement
             if event.key == pygame.K_LEFT:
                 player_xΔ = -0.3
             elif event.key == pygame.K_RIGHT:
                 player_xΔ = 0.3
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 player_yΔ = -0.3
             elif event.key == pygame.K_DOWN:
                 player_yΔ = 0.3
+            # Player Bullet Keyboard
+            elif event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    # Get current x coordinate of player
+                    bullet_x = player_x
+                    bullet_y = player_y
+                    fire_bullet(bullet_x, bullet_y)
+
+        # Release Keyboard
         if event.type == pygame.KEYUP:
             if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
                 player_xΔ = 0
@@ -85,6 +110,14 @@ while running:
     elif enemy_x >= width - l_enemy:
         enemy_xΔ = -0.3
         enemy_y += enemy_yΔ
+
+    # Player Bullet Movement
+    if bullet_y <= 0:
+        bullet_y = 480
+        bullet_state = "ready"
+    if bullet_state is "fire":
+        fire_bullet(bullet_x, bullet_y)
+        bullet_y -= bullet_yΔ
 
     show_player(player_x, player_y)
     show_enemy(enemy_x, enemy_y)
