3ca4fad0f453a640f56cac60bc8797aad8e49b70
Julian Giudice <juliangiudice@hotmail.com>
2023-01-27 09:50:11 -0300

    Created Boss Class - Part 7

--- bosses.py
+++ bosses.py
@@ -158,6 +158,11 @@ class Boss(pygame.sprite.Sprite):
             self.fire_cycles_counter = 0
             self.bullet_pattern_counter = 0
 
+        # Reset Animation when leaving Screen
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
+            self.enter_animation = True
+            self.rect.center = [WIDTH/2, -1/4 * HEIGHT]
+
 
 class BossBullet(pygame.sprite.Sprite):
 
@@ -209,18 +214,33 @@ bosses_bullet_group = pygame.sprite.Group()
 
 # Bosses - Category, Image, Scale, Movement Type, Velocity, HP, Bullet Type, Bullet Pattern Counter, Fire Cycles, Explosion Scale
 bosses = {
-    'boss_a': ['Images/Bosses/Captain_Death_Ship.png', (0.6, 0.6), 1, [0, 0], 100, ('a1', 'a2'), 200, (0.8, 0.8)],
-    'boss_b': ['Images/Bosses/General_Bugfix_Ship.png', (1.1, 1.1), 1, [0, 0], 125, [('b1', 'b2', 'b3'), ('a1', 'a2')], 300, [(0, 4), (200, 3)], (2.0, 2.0)],
+    'boss_a': ['Images/Bosses/Captain_Death_Ship.png', (0.6, 0.6), 1, [0, 0], 100, ('a3', 'a2'), 200, (0.8, 0.8)],
+    'boss_b': ['Images/Bosses/General_Bugfix_Ship.png', (1.1, 1.1), 1, [-1, 0], 125,
+               [['a2', 'b0', 'b2'], ['a1', 'b1'], ['b0'], ['b1'], ['b2'], ['b3'], ['b4'], ['b5'], ['b6'], ['b0'], ['a1'], ['a2'], ['a3'], ['a4'], ['a5'], ['a6'], ['b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6']], 200,
+               [(0, 5), (100, 4), (200, 2), (205, 2), (210, 2), (215, 2), (220, 2), (225, 2), (230, 2), (240, 2), (245, 2), (250, 2), (255, 2), (260, 2), (265, 2), (270, 2), (280, 3)],
+               (2.0, 2.0)],
     'boss_c': ['Images/Bosses/Crimson_Emperor_Ship.png', (0.8, 0.8), 1, [0, 0], 150, ('c1', 'c2'), 200, (0.9, 0.9)],
 }
 
 # Bosses Bullets - Image, Movement Type, Velocity, Angle, Sound, Explosion Sound
 bosses_bullets = {
-    'b_bullet_a1': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-1, 5], -10, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
-    'b_bullet_a2': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [1, 5], 10, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
-    'b_bullet_b1': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-6, 6], -45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
-    'b_bullet_b2': ['Images/Bosses_Bullets/enemy_bullet_F.png', 1, [0, 6], 0, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
-    'b_bullet_b3': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [6, 6], 45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a1': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [1, 5], 15, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a2': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [3, 5], 30, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a3': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [5, 5], 45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a4': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [5, 3], 60, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a5': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [5, 1], 75, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_a6': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [5, 0], 90, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+
+    'b_bullet_b0': ['Images/Bosses_Bullets/enemy_bullet_F.png', 1, [0, 5], 0, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b1': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-1, 5], -15, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b2': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-3, 5], -30, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b3': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-5, 5], -45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b4': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-5, 3], -60, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b5': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-5, 1], -75, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b6': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [-5, 0], -90, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b7': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [6, 6], 45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+    'b_bullet_b8': ['Images/Bosses_Bullets/enemy_bullet_F.png', 2, [6, 6], 45, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
+
     'b_bullet_c1': ['Images/Bosses_Bullets/enemy_bullet_F.png', 1, [0, 6], 0, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
     'b_bullet_c2': ['Images/Bosses_Bullets/enemy_bullet_F.png', 1, [0, 6], 0, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
     'b_bullet_c3': ['Images/Bosses_Bullets/enemy_bullet_F.png', 1, [0, 6], 0, 'Sounds/laser.wav', 'Sounds/explosion.wav'],
