c326637584c76b54612e63adfbb5438d190fa232
Julian Giudice <juliangiudice@hotmail.com>
2023-05-09 10:35:57 -0300

    Created Game Structure - Part 5 - Added Credits

--- main_menu.py
+++ main_menu.py
@@ -45,24 +45,28 @@ class Menu(BaseState):
         ]
         self.credits = [
             TextCreator(0, "BACK",
-                        'freesansbold.ttf', 48, 52, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
+                        'freesansbold.ttf', 48, 48, (255, 255, 255), (193, 225, 193),
+                        (WIDTH / 2, 5/6 * HEIGHT), "", 50),
             TextCreator(1, "May 2023", 'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
             TextCreator(2,
                         "Game Project is a Python game developed by me: Julian Giudice (github.com/karpo27).",
                         'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
             TextCreator(3, "It's my first game since I started learning Python 6 months ago.",
                         'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
             TextCreator(4,
-                        "I want to thank Pygame for allowing me to use this module and allow so many users to code their games.",
+                        "I want to thank Pygame for allowing me to use this module,",
                         'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
-            TextCreator(5, "I'll keep coding and trying myself harder to incorporate to the IT world. ",
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
+            TextCreator(5,
+                        "and allow so many users to code their games.",
                         'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
-                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 50),
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
+            TextCreator(6, "I'll keep coding and trying myself harder to incorporate to the IT world. ",
+                        'freesansbold.ttf', 22, 94, (255, 255, 255), (193, 225, 193),
+                        (WIDTH / 2, 1 / 3 * HEIGHT), "", 40),
         ]
 
         # Title and Icon:
@@ -129,6 +133,17 @@ class Menu(BaseState):
         elif self.index < 0:
             self.index = self.options_qty
 
+    def rotate_pointer(self, position):
+        # Render Player Icon Rotation:
+        rot_player_img = pygame.transform.rotozoom(self.image, self.angle, 1)
+        rot_player_img_rect = rot_player_img.get_rect()
+        rot_player_img_position = (
+            position.text_position[0] - self.rect.x - rot_player_img_rect.width / 2,
+            position.text_position[1] + self.init_pos_y - rot_player_img_rect.height / 2
+        )
+        SCREEN.blit(rot_player_img, rot_player_img_position)
+        self.angle += 2.2
+
     def draw(self, surface):
         # Draw Scrolling Background:
         background_main_menu.update()
@@ -138,19 +153,13 @@ class Menu(BaseState):
             self.title.render_text(-1)
             for text in self.menu_options:
                 text.render_text(self.index)
+            self.rotate_pointer(self.menu_options[self.index])
         elif self.screen == "OPTIONS":
             for text in self.options_options:
                 text.render_text(self.index)
+            self.rotate_pointer(self.menu_options[self.index])
         elif self.screen == "CREDITS":
             for text in self.credits:
-                text.render_text(0)
+                text.render_text(self.index)
+            self.rotate_pointer(self.credits[self.index])
 
-        # Render Player Icon Rotation Animation:
-        rot_player_img = pygame.transform.rotozoom(self.image, self.angle, 1)
-        rot_player_img_rect = rot_player_img.get_rect()
-        rot_player_img_position = (
-            self.menu_options[self.index].text_position[0] - self.rect.x - rot_player_img_rect.width / 2,
-            self.menu_options[self.index].text_position[1] + self.init_pos_y - rot_player_img_rect.height / 2
-        )
-        SCREEN.blit(rot_player_img, rot_player_img_position)
-        self.angle += 2.2
